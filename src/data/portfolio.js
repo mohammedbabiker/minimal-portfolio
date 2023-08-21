f0b68688f5bb8b7baa0f1be8f90609bd1da650d0
mohammedbabiker <mobabikerbabai20@gmail.com>
2023-08-21 15:14:10 +0300

    update Portfolio section

---
+++ src/data/portfolio.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    title: "Terminal Website",
+    imgUrl: "https://picsum.photos/seed/picsum/200/300",
+    stack: ["HTML", "CSS", "JavaScript"],
+    link: "https://www.terminal",
+  },
+  {
+    title: "Developer Portfolio",
+    imgUrl: "https://picsum.photos/200/300/?blur",
+    stack: ["React", "Tailwind", "Vite"],
+    link: "https://www.terminal",
+  },
+  {
+    title: "AI Ferried",
+    imgUrl: "https://picsum.photos/id/870/200/300?grayscale&blur=2",
+    stack: ["Python", "C++", "Robotics"],
+    link: "https://www.terminal",
+  },
+];
