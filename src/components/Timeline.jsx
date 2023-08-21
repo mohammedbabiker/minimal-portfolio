77f32c7e883e8361013ec98571f968ad20147569
mohammedbabiker <mobabikerbabai20@gmail.com>
2023-08-21 16:26:00 +0300

    update timeline section

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -1,8 +1,21 @@
 import React from "react";
+import timeline from "../data/timeline";
+import TimelineItem from "./TimelineItem";
 function Timeline() {
   return (
-    <div>
-      <h1>Timeline</h1>
+    <div className="flex flex-col md:flex-row justify-center my-20">
+      <div className="w-full md:w-7/12">
+        {timeline.map((item) => (
+          <TimelineItem
+            key={item.id}
+            item={item}
+            years={item.years}
+            title={item.title}
+            duration={item.duration}
+            details={item.details}
+          />
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/data/timeline.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    years: "Today",
+    title: "Youtube Content",
+    duration: "2 years",
+    details:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation laborum.",
+  },
+  {
+    years: "Today",
+    title: "Solo Indie Content",
+    duration: "2 years",
+    details:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation laborum.",
+  },
+  {
+    years: "2023",
+    title: "Bachelor Degree in Accounting",
+    duration: "2 years",
+    details:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation laborum.",
+  },
+  {
+    years: "2022",
+    title: "Front-End Software Development",
+    duration: "2 years",
+    details:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation laborum.",
+  },
+  {
+    years: "2020",
+    title: "First line in Code",
+    duration: "2 years",
+    details:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation laborum.",
+  },
+];
